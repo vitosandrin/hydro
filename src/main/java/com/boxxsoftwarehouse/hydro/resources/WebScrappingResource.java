728faa45b9c8ee681d0f64dbd7c344104a83811d
Vito <sandrinvito10@gmail.com>
2023-08-24 00:48:40 -0300

    working: test select elements

--- src/main/java/com/boxxsoftwarehouse/hydro/resources/WebScrappingResource.java
+++ src/main/java/com/boxxsoftwarehouse/hydro/resources/WebScrappingResource.java
@@ -18,18 +18,13 @@ public class WebScrappingResource {
 	public ResponseEntity<String> findFirstSearchResult(@RequestParam("term") String searchTerm) {
 		Document document = this.webScrappingService.searchOnGoogleShop(searchTerm);
 
-		String title = document.select(
-				"div.sh-dgr__gr-auto:nth-child(7) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > a:nth-child(1) > div:nth-child(1) > h3:nth-child(1)")
-				.first().text();
+		String title = document.select("a.Lq5OHe:nth-child(2) > div:nth-child(1) > h3:nth-child(1)").first().text();
 		String price = document.select("span.QIrs8:nth-child(4)").first().text();
-		String ratingValue = document.select(
-				"div.sh-dgr__gr-auto:nth-child(7) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > a:nth-child(1) > div:nth-child(2) > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)")
-				.first().text();
-		String merchan = document.select(
-				"span.eaGTj:nth-child(2) > div:nth-child(2) > a:nth-child(1) > div:nth-child(3)")
-				.first().text();
+		String merchan = document
+				.select("span.eaGTj:nth-child(2) > div:nth-child(2) > a:nth-child(1) > div:nth-child(3)").first()
+				.text();
 
 		return ResponseEntity.ok().body("First Result Title: " + title + " and your price is: " + price
-				+ " your rating value is: " + ratingValue + " Owner " + merchan);
+				+ " your owner is: " + merchan);
 	}
 }
