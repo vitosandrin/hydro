934fc6cf759111b49a3fc6695cf63611bb33b663
Vito <sandrinvito10@gmail.com>
2023-08-24 00:18:53 -0300

    working: test select elements

--- src/main/java/com/boxxsoftwarehouse/hydro/resources/WebScrappingResource.java
+++ src/main/java/com/boxxsoftwarehouse/hydro/resources/WebScrappingResource.java
@@ -19,7 +19,10 @@ public class WebScrappingResource {
 	public ResponseEntity<String> findFirstSearchResult(@RequestParam("term") String searchTerm) {
 		Document document = this.webScrappingService.searchOnGoogleShop(searchTerm);
 
-		String title = document.getElementsByClass("tAxDx").first().text();
-		return ResponseEntity.ok().body("First Result Title: " + title);
+		String title = document.select(
+				"div.sh-dgr__gr-auto:nth-child(7) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2) > a:nth-child(1) > div:nth-child(1) > h3:nth-child(1)")
+				.first().text();
+		String price = document.select("span.QIrs8:nth-child(4)").first().text();
+		return ResponseEntity.ok().body("First Result Title: " + title + " and your price is: " + price);
 	}
 }
